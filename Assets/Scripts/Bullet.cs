73fca8093c0ff0939de9b395aff73730671f7f6d
Lui <luifr@usp.br>
2018-04-21 14:52:40 -0300

    Script Bullet

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Mouse0)){
+            RaycastHit hit;
+            if (Physics.Raycast(Camera.main.transform.position,Camera.main.transform.forward,out hit)){
+                print(hit.collider);
+            }
+        }
+	}
+}
