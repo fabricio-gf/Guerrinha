f0b9d8c71d6c01729004d1d6075819428774b937
Oliver <oliverbecker@usp.br>
2018-04-22 04:27:45 +0000

    Main menu, creditos e pause menu implementados
    Trabalhando no menu de opções e background.

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MenuManager {
+
+	public GameObject InGameMenu;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Escape)) {
+			if (InGameMenu.activeInHierarchy)
+				CloseMenu ();
+			else
+				OpenMenu ();
+		}
+	}
+	public void OpenMenu() {
+		Time.timeScale = 0f;
+		InGameMenu.SetActive (true);
+	}
+
+	public void CloseMenu() {
+		Time.timeScale = 1f;
+		InGameMenu.SetActive (false);
+	}
+}
