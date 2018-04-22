fb22d5034b2d7542d9877f5c8ce1beef0c2dc511
Lui <luifr@usp.br>
2018-04-21 22:34:32 -0300

    Balas implementadas

---
+++ Assets/Scripts/BulletMovement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMovement : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+}
---
+++ Assets/Scripts/InstantiateBullet.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateBullet : MonoBehaviour{
+
+    public GameObject sphere;
+    public float force = 100;
+
+    // Use this for initialization
+    void Start(){
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+        if (Input.GetButtonDown("Fire1"))
+        {
+            GameObject project = Instantiate(sphere,Camera.main.transform.position + Camera.main.transform.forward , Camera.main.transform.rotation);
+            project.GetComponent<Rigidbody>().AddForce(Camera.main.transform.forward * force , ForceMode.Impulse);
+            Destroy(project, 2f);
+        }
+    }
+}
