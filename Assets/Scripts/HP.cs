d6bd21578991728e70243211e47f10e902515d4e
gael-sng <gael.sng@gmail.com>
2018-04-21 23:36:50 -0300

    inimigo parcialmente implementado, e hp começado

--- Assets/Scripts/HP.cs
+++ Assets/Scripts/HP.cs
@@ -3,14 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class HP : MonoBehaviour {
+	private float _actualHP;
+	[SerializeField] private float _MaxHP = 100f;
 
 	// Use this for initialization
 	void Start () {
-		
+		_actualHP = _MaxHP;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	public void TakeDamage(float damage){
+		_actualHP -= damage;
+		if (_actualHP <= 0f) {
+			Destroy (gameObject);
+		}
 	}
 }
