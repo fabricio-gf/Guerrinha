9711c4ca54588217c34a642dcc671e9d4d2fbe49
Oliver <oliverbecker@usp.br>
2018-04-24 01:15:00 +0000

    Cena de menu de volta e pause desabilita rotação.

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -3,20 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class PauseMenu : MenuManager {
 
 	public GameObject InGameMenu;
 	public GameObject OptionMenu;
 
+	public FirstPersonController player;
+
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown (KeyCode.Escape)) {
+		if (Input.GetKeyDown (KeyCode.Escape) && !OptionMenu.activeInHierarchy) {
 			if (InGameMenu.activeInHierarchy)
 				CloseMenu ();
 			else
@@ -24,11 +27,13 @@ public class PauseMenu : MenuManager {
 		}
 	}
 	public void OpenMenu() {
+		player.enabled = false;
 		Time.timeScale = 0f;
 		InGameMenu.SetActive (true);
 	}
 
 	public void CloseMenu() {
+		player.enabled = true;
         Time.timeScale = 1f;
 		InGameMenu.SetActive (false);
 	}
